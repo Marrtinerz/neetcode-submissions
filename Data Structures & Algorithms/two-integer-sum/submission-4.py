8455d22495180bd17c35870da32875993d66ad4a
Martins Nnamchi <martins@siftinteractive.com>
2026-08-08 23:09:27 +0100

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        store = {}
+        for index, value in enumerate(nums):
+            num_to_find = target - value
+            if num_to_find in store:
+                return [store[num_to_find], index]
+            else:
+                store[value] = index
+            
+        return []
